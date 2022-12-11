8fb4cdf58b1e6acdfc15ffcf059c469d6d2cba40
y045u <fukasawa0405.tech@gmail.com>
2022-12-12 02:55:32 +0900

    form

---
+++ form/input.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<head></head>
+<body>
+
+<form method="GET" action="input.php">
+氏名
+<input type="text" name="your_name">
+
+<input type="submit" name="送信">
+
+</form>
+
+</body>
+</html>
