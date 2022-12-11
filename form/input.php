e2b801a3b47e3b6117a5dc07086e7ae6f0969405
y045u <fukasawa0405.tech@gmail.com>
2022-12-12 03:19:49 +0900

    GETとPOST

--- form/input.php
+++ form/input.php
@@ -1,11 +1,27 @@
+<?php
+
+if(!empty($_POST)){
+    echo '<pre>';
+    var_dump($_POST);
+    echo '</pre>';
+}
+// スーパーグローバル変数 php 9種類
+// 連想配列 
+
+?>
+
 <!DOCTYPE html>
 <meta charset="utf-8">
 <head></head>
 <body>
 
-<form method="GET" action="input.php">
+<form method="POST" action="input.php">
 氏名
 <input type="text" name="your_name">
+<br>
+<input type="checkbox" name="sports[]" value="野球">野球
+<input type="checkbox" name="sports[]" value="サッカー">サッカー
+<input type="checkbox" name="sports[]" value="バスケ">バスケ
 
 <input type="submit" name="送信">
 
