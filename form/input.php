23fb912444a991c3bd9d3ef66cb1faf4d6ad60f9
y045u <fukasawa0405.tech@gmail.com>
2022-12-14 01:21:17 +0900

    入力、確認、完了画面作成

--- form/input.php
+++ form/input.php
@@ -1,12 +1,23 @@
 <?php
-
+// スーパーグローバル変数 php 9種類
+// 連想配列 
 if(!empty($_POST)){
     echo '<pre>';
     var_dump($_POST);
     echo '</pre>';
 }
-// スーパーグローバル変数 php 9種類
-// 連想配列 
+
+
+// 入力、確認、完了で分ける場合はinput.php, confirm.php, thanks.phpのようにやるか、input.phpにすべてまとめる
+
+$pageFlag = 0;
+
+if(!empty($_POST['btn_confirm'])){
+    $pageFlag = 1;
+}
+if(!empty($_POST['btn_submit'])){
+    $pageFlag = 2;
+}
 
 ?>
 
@@ -15,17 +26,41 @@ if(!empty($_POST)){
 <head></head>
 <body>
 
+<?php if($pageFlag === 0) : ?>
+
 <form method="POST" action="input.php">
 氏名
 <input type="text" name="your_name">
 <br>
-<input type="checkbox" name="sports[]" value="野球">野球
-<input type="checkbox" name="sports[]" value="サッカー">サッカー
-<input type="checkbox" name="sports[]" value="バスケ">バスケ
+メールアドレス
+<input type="email" name="email">
+<br>
+<input type="submit" name="btn_confirm" value="確認する">
 
-<input type="submit" name="送信">
+<?php endif; ?>
 
-</form>
+
+<?php if($pageFlag === 1) : ?>
+
+<form method="POST" action="input.php">
+氏名
+<?php echo $_POST['your_name'] ;?>
+<br>
+メールアドレス
+<?php echo $_POST['email'] ;?>
+<br>
+<input type="submit" name="btn_submit" value="送信する">
+<input type="hidden" name="your_name" value="<?php echo $_POST['your_name'] ;?>">
+<input type="hidden" name="email" value="<?php echo $_POST['email'] ;?>">
+
+<?php endif; ?>
+
+
+<?php if($pageFlag === 2) : ?>
+
+送信が完了しました。
+
+<?php endif; ?>
 
 </body>
 </html>
---
+++ form/tmp.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<head></head>
+<body>
+
+<form method="POST" action="input.php">
+氏名
+<input type="text" name="your_name">
+<br>
+<input type="checkbox" name="sports[]" value="野球">野球
+<input type="checkbox" name="sports[]" value="サッカー">サッカー
+<input type="checkbox" name="sports[]" value="バスケ">バスケ
+
+<input type="submit" name="送信">
+
+</form>
+
+</body>
+</html>
