32b03ed5fd3ceb62d4c8fe0af6e169c4b78b2c68
y045u <fukasawa0405.tech@gmail.com>
2022-12-14 03:07:25 +0900

    クリックジャッキングとCSRF対策

--- form/input.php
+++ form/input.php
@@ -1,11 +1,16 @@
 <?php
+
+session_start();
+
+header("X-FRAME-OPTIONS: DENY");
+
 // スーパーグローバル変数 php 9種類
 // 連想配列 
-// if(!empty($_POST)){
-//     echo '<pre>';
-//     var_dump($_POST);
-//     echo '</pre>';
-// }
+if(!empty($_SESSION)){
+    echo '<pre>';
+    var_dump($_SESSION);
+    echo '</pre>';
+}
 
 
 // 入力、確認、完了で分ける場合はinput.php, confirm.php, thanks.phpのようにやるか、input.phpにすべてまとめる
@@ -33,6 +38,14 @@ function h($str)
 
 <?php if($pageFlag === 0) : ?>
 
+<?php
+if(!isset($_SESSION['$csrfToken'])){
+    $csrfToken = bin2hex(random_bytes(32));
+    $_SESSION['csrfToken'] = $csrfToken;
+}
+$token = $_SESSION['csrfToken'];
+?>
+
 <form method="POST" action="input.php">
 氏名
 <input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])){echo h($_POST['your_name']) ;}?>">
@@ -41,12 +54,13 @@ function h($str)
 <input type="email" name="email" value="<?php if(!empty($_POST['email'])){echo h($_POST['email']) ;}?>">
 <br>
 <input type="submit" name="btn_confirm" value="確認する">
+<input type="hidden" name="csrf" value="<?php echo $token; ?>">
 
 <?php endif; ?>
 
 
 <?php if($pageFlag === 1) : ?>
-
+<?php if($_POST['csrf'] === $_SESSION['csrfToken']) :?>
 <form method="POST" action="input.php">
 氏名
 <?php echo h($_POST['your_name']) ;?>
@@ -58,14 +72,19 @@ function h($str)
 <input type="submit" name="btn_submit" value="送信する">
 <input type="hidden" name="your_name" value="<?php echo h($_POST['your_name']) ;?>">
 <input type="hidden" name="email" value="<?php echo h($_POST['email']) ;?>">
+<input type="hidden" name="csrf" value="<?php echo h($_POST['csrf']) ;?>">
 
+<?php endif; ?>
 <?php endif; ?>
 
 
 <?php if($pageFlag === 2) : ?>
+<?php if($_POST['csrf'] === $_SESSION['csrfToken']) :?>
 
 送信が完了しました。
 
+<?php unset($_SESSION['csrfToken']); ?>
+<?php endif; ?>
 <?php endif; ?>
 
 </body>
