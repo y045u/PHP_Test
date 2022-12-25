ef9dd2f2a10ce31369a975494271257239cee1df
y045u <fukasawa0405.tech@gmail.com>
2022-12-26 03:48:57 +0900

    入力画面のレイアウト（氏名、メールアドレス、ホームページ）

--- form/input.php
+++ form/input.php
@@ -40,9 +40,18 @@ function h($str)
 
 ?>
 
-<!DOCTYPE html>
-<meta charset="utf-8">
-<head></head>
+<!doctype html>
+<html lang="ja">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
 <body>
 
 
@@ -66,15 +75,27 @@ $token = $_SESSION['csrfToken'];
 <?php echo '</ul>' ;?>
 <?php endif ;?>
 
-<form method="POST" action="input.php">
-氏名
-<input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])){echo h($_POST['your_name']) ;}?>">
-<br>
-メールアドレス
-<input type="email" name="email" value="<?php if(!empty($_POST['email'])){echo h($_POST['email']) ;}?>">
-<br>
-ホームぺージ
-<input type="url" name="url" value="<?php if(!empty($_POST['url'])){echo h($_POST['url']) ;}?>">
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <form method="POST" action="input.php">
+            <div class="from-group">
+                <label for="your_name">氏名</label>
+                <input type="text" class="form-control" id="your_name" name="your_name" value="<?php if(!empty($_POST['your_name'])){echo h($_POST['your_name']) ;}?>" required>
+            </div>
+
+            <div class="from-group">
+                <label for="email">メールアドレス</label>
+                <input type="email" class="form-control" id="email" name="email" value="<?php if(!empty($_POST['email'])){echo h($_POST['email']) ;}?>">
+            </div>
+
+            <div class="from-group">
+                <label for="url">ホームページ</label>
+                <input type="url" class="form-control" id="url" name="url" value="<?php if(!empty($_POST['url'])){echo h($_POST['url']) ;}?>">
+            </div>
+        </div>
+    </div>
+</div>
 <br>
 性別
 <input type="radio" name="gender" value="0" <?php if(isset($_POST['gender']) && $_POST['gender'] === "0"){echo 'checked'; }?>>男性
@@ -158,5 +179,10 @@ $token = $_SESSION['csrfToken'];
 <?php endif; ?>
 <?php endif; ?>
 
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </body>
 </html>
